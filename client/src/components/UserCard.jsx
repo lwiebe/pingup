9ba5bbe8e0e0edb25cffd2c73d6d051c63f04372
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-08-26 14:10:48 +0200

    project completed

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -1,17 +1,53 @@
 import React from 'react'
 import { dummyUserData } from '../assets/assets'
 import { MapPin, MessageCircle, Plus, UserPlus } from 'lucide-react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useAuth } from '@clerk/clerk-react'
+import { useNavigate } from 'react-router-dom'
+import api from '../api/axios'
+import toast from 'react-hot-toast'
+import { fetchUser } from '../features/user/userSlice'
 
 const UserCard = ({user}) => {
 
-    const currentUser = dummyUserData
+    const currentUser = useSelector((state)=>state.user.value)
+    const {getToken} = useAuth()
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
-    const handleFollowe = async () => {
-
+    const handleFollow = async () => {
+      try {
+        const { data } = await api.post('/api/user/follow', {id: user._id}, {
+          headers: {Authorization: `Bearer ${await getToken()}`}
+        })
+        if(data.success){
+          toast.success(data.message)
+          dispatch(fetchUser(await getToken()))
+        }else{
+          toast.error(data.message)
+        }
+      } catch (error) {
+        toast.error(error.message)
+      }
     }
 
     const handleConnectionRequest = async () => {
+      if(currentUser.connections.includes(user._id)){
+        return navigate('/messages/' + user._id)
+      }
 
+      try {
+        const { data } = await api.post('/api/user/connect', {id: user._id}, {
+          headers: {Authorization: `Bearer ${await getToken()}`}
+        })
+        if(data.success){
+          toast.success(data.message)
+        }else{
+          toast.error(data.message)
+        }
+      } catch (error) {
+        toast.error(error.message)
+      }
     }
 
   return (
@@ -34,7 +70,7 @@ const UserCard = ({user}) => {
 
       <div className='flex mt-4 gap-2'>
         {/* Follow Button */}
-        <button onClick={handleFollowe} disabled={currentUser?.following.includes(user._id)} className='w-full py-2 rounded-md flex justify-center items-center gap-2 bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition text-white cursor-pointer'>
+        <button onClick={handleFollow} disabled={currentUser?.following.includes(user._id)} className='w-full py-2 rounded-md flex justify-center items-center gap-2 bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 active:scale-95 transition text-white cursor-pointer'>
             <UserPlus className='w-4 h-4'/> {currentUser?.following.includes(user._id) ? 'Following' : 'Follow'}
         </button>
         {/* Connection Request Button / Message Button */}
